db39cb4d6e72c243d0c974fc510a3470cfb75603
devkatharine <devkatharine@gmail.com>
2022-03-04 15:17:47 -0300

    Atualizei aula04

---
+++ exercicios/aula04/scripts.js
@@ -0,0 +1,8 @@
+var nome = window.prompt('Qual é o seu nome?');
+document.write(`Olá <strong>${nome}</strong>. Seu nome tem ${nome.length} letras.`);
+//Formatação de Numeros
+// var n1 = 1543.5
+//n1
+//n1.tofixed(2) 1543.50
+// n1.toLocalestring('pt-BR', {style: 'currency, currency: 'BRL'}) 
+// R$1.545,50
