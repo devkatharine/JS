ac9240d8c9e12925a9f3493fe86e3d67fc4fd55c
devkatharine <devkatharine@gmail.com>
2022-03-04 13:18:56 -0300

    Initial commit

---
+++ exercicios/aula06/script.js
@@ -0,0 +1,2 @@
+var nome = window.prompt('Qual é o seu nome?');//Emite uma pergunta e resposta
+window.alert('É um grande prazer em te conhecer, ' +nome);
---
+++ exercicios/aulo04/scripts.js
@@ -0,0 +1,3 @@
+window.alert('Minha primeira mensagem');//Emite uma mensagem.
+window.confirm('Está gostando de JS?');//Emite uma mensagem de confirmação
+window.prompt('Qual é o seu nome?');//Emite uma pergunta e resposta
