df725c7fd78087f2f07ecf01b9579a48e5ce2ddf
devkatharine <devkatharine@gmail.com>
2022-03-09 10:46:34 -0300

    Atualizei aula04.

--- exercicios/aula04/scripts.js
+++ exercicios/aula04/scripts.js
@@ -6,3 +6,15 @@ document.write(`Olá <strong>${nome}</strong>. Seu nome tem ${nome.length} letra
 //n1.tofixed(2) 1543.50
 // n1.toLocalestring('pt-BR', {style: 'currency, currency: 'BRL'}) 
 // R$1.545,50
+//Aritméticos
+//5 + 2 = 7 soma
+//5 - 2 = 3 subtração
+//5 * 2 = 10 multiplicação
+//5 / 2 = 2.5 divisão
+//5 % 2 = 1 resto da divisão
+//5 ** 2 = 25 cinco ao quadrado potencia
+//Precedência ordem
+//()
+//** 
+//* / %
+//+ -
