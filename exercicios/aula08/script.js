ca988e3a871cd10af72de6d863c076f248fdb505
devkatharine <devkatharine@gmail.com>
2022-03-17 19:23:34 -0300

    three new classes were created

---
+++ exercicios/aula08/script.js
@@ -0,0 +1,6 @@
+var velocidade= 80
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+if(velocidade > 60){
+    console.log('Você ultrapassou o limite de velocidade. MULTADO')
+}
+console.log('Dirija sempre usando o cinto de segurança')
