cb73e041db2db4334106e450b8bf0ac132fbd5a7
devkatharine <devkatharine@gmail.com>
2022-03-21 16:39:23 -0300

    created class 12
    function

---
+++ exercicios/aula012/functions.js
@@ -0,0 +1,9 @@
+function createPhrases(){
+    console.log('Estudar é muito gratificante.')
+    console.log('Paciência e persistência.')
+    console.log('Revisão é a mãe do aprendizado.')
+}
+
+createPhrases()
+
+console.log('fim do programa')
