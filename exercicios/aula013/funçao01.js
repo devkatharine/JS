4055487bc86771274d298308eade7a9416ab02b6
devkatharine <devkatharine@gmail.com>
2022-03-25 11:35:20 -0300

    created class 13

---
+++ exercicios/aula013/funçao01.js
@@ -0,0 +1,11 @@
+function parimpar(numero){
+    if(numero%2==0){
+        return 'par'
+    }
+    else{
+        return 'impar'
+    }
+}
+
+let res = parimpar(2)
+console.log(res)
---
+++ exercicios/aula013/funçao03.js
@@ -0,0 +1,4 @@
+let k = function(x){
+    return x*2
+}
+console.log(k(5))
