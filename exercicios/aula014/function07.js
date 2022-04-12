6c9f4a2bac6b68fdb183ac4b95426b3746cb6509
devkatharine <devkatharine@gmail.com>
2022-04-12 17:17:55 -0300

    new js exercises

---
+++ exercicios/aula014/function07.js
@@ -0,0 +1,11 @@
+const soma = function(numero1, numero2){
+    let total = numero1 + numero2
+    return total
+}
+
+let numero1 = 10
+let numero2 = 20
+
+console.log(`O número 1 é ${numero1}`)
+console.log(`O número 2 é ${numero2}`)
+console.log(`A soma é ${soma(numero1, numero2)}`)
---
+++ exercicios/aula014/function08.js
@@ -0,0 +1,7 @@
+function numeroPos(numNegativo){
+    return Math.abs(numNegativo);
+}
+
+console.log(numeroPos(-13));
+console.log(numeroPos(-31));
+console.log(numeroPos(-21,14));
