16c9b3639e0c2ab7d8c685f74ecc9d4f8d55b7a6
devkatharine <devkatharine@gmail.com>
2022-06-29 16:33:59 -0300

    added school grade system

---
+++ exercicios/aula19/functions01.js
@@ -0,0 +1,15 @@
+const sum = function(number1, number2){
+  let total = number1 + number2
+  return total
+}
+
+let numberA = 20
+let numberB = 10
+
+console.log(`o número 1 é ${numberA}`)
+console.log(`o número 1 é ${numberB}`)
+console.log(`a soma é ${sum(numberA, numberB)}`)
+
+
+
+
---
+++ exercicios/exercicios-praticos/sistema-de-notas/scripts.js
@@ -0,0 +1,13 @@
+let grades = 
+
+if(grades >= 90){
+  console.log('Your grade is A')
+} else if (grades >= 80 && grades <= 89){
+  console.log('Your grade is B')
+} else if (grades >= 70 && grades <= 79) {
+  console.log('Your grade is C')
+} else if (grades >= 60 && grades <= 69){
+  console.log('Your grade is D')
+} else {
+  console.log('Your grade is F')
+}
