6332d2fa9fca2f7693dcd430483fffc5cef1ff8a
devkatharine <devkatharine@gmail.com>
2022-07-01 16:00:26 -0300

    new exercises added

---
+++ exercicios/aula20/objeto.js
@@ -0,0 +1,13 @@
+let carro = {
+  marca: "Ford",
+  portas: 4,
+  eletrico: false,
+  motor: 3.0
+
+}
+
+console.log(carro.portas);
+delete carro.portas;
+console.log(carro.portas);
+carro.tetoSolar = true;
+console.log(carro.tetoSolar);
