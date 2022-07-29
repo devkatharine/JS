edfa639a3651cb78ccde0df2f856a3ac947a9425
devkatharine <devkatharine@gmail.com>
2022-07-29 16:27:21 -0300

    updated exercise

--- exercicios/secao10/manipulando-classes.js
+++ exercicios/secao10/manipulando-classes.js
@@ -1,18 +1,27 @@
-function verde(){
-  document.querySelector('#exemplo').classList.remove('vermelho');
-  document.querySelector('#exemplo').classList.remove('azul');
-  document.querySelector('#exemplo').classList.add('verde');
-
+function verde() {
+  document.querySelector('#exemplo').classList.remove('vermelho')
+  document.querySelector('#exemplo').classList.remove('azul')
+  document.querySelector('#exemplo').classList.add('verde')
 }
 
-function vermelho(){
-  document.querySelector('#exemplo').classList.remove('verde');
-  document.querySelector('#exemplo').classList.remove('azul');
-  document.querySelector('#exemplo').classList.add('vermelho');
+function vermelho() {
+  document.querySelector('#exemplo').classList.remove('verde')
+  document.querySelector('#exemplo').classList.remove('azul')
+  document.querySelector('#exemplo').classList.add('vermelho')
 }
 
-function azul(){
-  document.querySelector('#exemplo').classList.remove('verde');
-  document.querySelector('#exemplo').classList.remove('vermelho');
-  document.querySelector('#exemplo').classList.add('azul');
+function azul() {
+  document.querySelector('#exemplo').classList.remove('verde')
+  document.querySelector('#exemplo').classList.remove('vermelho')
+  document.querySelector('#exemplo').classList.add('azul')
+}
+
+function trocar() {
+  if (document.querySelector('#butbut').classList.contains('preto')) {
+    document.querySelector('#butbut').classList.remove('preto')
+    document.querySelector('#butbut').classList.add('verde')
+  } else {
+    document.querySelector('#butbut').classList.remove('verde')
+    document.querySelector('#butbut').classList.add('preto')
+  }
 }
